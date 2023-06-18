11d7cbfebc6fdd6142e666a8f4082029e93e860f
Muhammet Furkan Küpçü <51542849+furkankupcu@users.noreply.github.com>
2023-06-18 22:04:19 +0300

    Frontend deleted

--- BoschFrontend/src/app/model/injector.ts
+++
@@ -1,7 +0,0 @@
-export interface BB_Injector {
-    Injector_ID : number, 
-    ModelNumber : string,
-    InjectorPrice : number,
-    InjectorStatus : string,
-    OrderId : number
-}
--- BoschFrontend/src/app/model/subcomponentypes.ts
+++
@@ -1,5 +0,0 @@
-export interface Subcomponentypes {
-    stationId: number,
-    subcomponentCost: number,
-    subcomponentType: number
-}
